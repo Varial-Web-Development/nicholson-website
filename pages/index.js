e806e5741af23232e9c3f02334fc9b15a537a84a
Danny Barrett <jamesdbarrett1992@gmail.com>
2022-07-23 14:19:57 -0700

    added title and description to home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Layout from "../components/layouts/standard-page";
 
 export default function HomePage() {
   return (
     <Layout>
+      <Head>
+        <title>Local Independent Insurance Agency - Nicholson & Associates</title>
+        <meta name="description" content="my description" />
+      </Head>
       <main>
         hello world
       </main>

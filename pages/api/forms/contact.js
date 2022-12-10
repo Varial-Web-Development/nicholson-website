358e659b265443ff5c30cb69f52039530cd7a345
Danny Barrett <jamesdbarrett1992@gmail.com>
2022-12-10 14:58:38 -0800

    Adding logging for contact and free quote requests

--- pages/api/forms/contact.js
+++ pages/api/forms/contact.js
@@ -14,115 +14,121 @@ export default async function handler(req, res) {
     key: API_KEY,
   })
 
-  const now = new Date()
+  const mongo = new MongoClient(process.env.MONGO_URI)
 
-  if (body.newsletter) {
-    const mongo = new MongoClient(process.env.MONGO_URI)
+  try {
+    await mongo.connect()
+    const client = mongo.db('VarialCMS')
 
-    try {
-      await mongo.connect()
-      const client = mongo.db('VarialCMS')
+    const now = new Date()
 
-      // const listExists = await MailingList.count({ name: 'VarialCMS' })
-      const listExists = await client.collection('mailing_lists').countDocuments({ name: 'VarialCMS' })
-      let varialCmsList
-  
-      if (listExists === 0) {
-        // varialCmsList = new MailingList({
-        //   name: 'VarialCMS'
-        // })
-  
-        // await varialCmsList.validate()
-        // await varialCmsList.save()
-        await client.collection('mailing_lists').insertOne({
-          name: 'VarialCMS',
-        })
-
-        varialCmsList = await client.collection('mailing_lists').findOne({ name: 'VarialCMS' })
-      } else {
-        varialCmsList = await client.collection('mailing_lists').findOne({ name: 'VarialCMS' })
-      }
-  
-      const contactExists = await client.collection('contacts').countDocuments({ email: body.email.toLowerCase() })
-  
-      if (contactExists === 0) {
-        // const newContact = new Contact({
-        //   name: body.name,
-        //   email: body.email.toLowerCase(),
-        //   mailingLists: [varialCmsList._id],
-        // })
+    if (body.newsletter) {
+        const listExists = await client.collection('mailing_lists').countDocuments({ name: 'VarialCMS' })
+        let varialCmsList
     
-        // await newContact.validate()
-        // await newContact.save()
-        await client.collection('contacts').insertOne({
-          name: body.name,
-          email: body.email,
-          mailingLists: [varialCmsList._id]
-        })
-      } else {
-        const contact = await client.collection('contacts').findOne({ email: body.email.toLowerCase() })
-  
-        if (contact.mailingLists.some(i => JSON.stringify(i) === JSON.stringify(varialCmsList._id))) {
-          // do nothing
-        } else {
-          // contact.mailingLists = [...contact.mailingLists, varialCmsList._id]
-          // await contact.validate()
-          // await contact.save()
-          await client.collection('contacts').updateOne({ _id: contact._id }, {
-            $set: {
-              mailingLists: [...contact.mailingLists, varialCmsList._id]
-            }
+        if (listExists === 0) {
+          await client.collection('mailing_lists').insertOne({
+            name: 'VarialCMS',
           })
-        }
-      }
 
-    } catch (error) {
-      console.error('Mongo error', error)
-    }
-    // await mongoose.connect(process.env.MONGO_URI)
-
-    
-  }
-
-  const customerEmail = {
-    from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
-    to: body.email,
-    subject: 'Contact request received',
-    html: `
-      <h1>Your contact request has been received!</h1>
-      <p>Thank you for choosing Nicholson & Associates! Our team will be in touch with you soon.</p> 
-    `
-  }
-
-
-  const staffEmail = {
-    from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
-    to: process.env.MAIL_TO,
-    subject: `Contact request received at ${now}`,
-    html: `
-        <p>Date: ${now}</p>
-        <p>Name: ${body.name}</p>
-        <p>Email: ${body.email}</p>
-        <p>Phone: ${body.phone}</p>
-        <p>City: ${body.city}</p>
-        <p>Referred by: ${body.referredBy}</p>
-        <p>Questions/Comments: ${body.comments}</p>
-      `
-  }
-
-  await mg.messages.create(DOMAIN, staffEmail)
-  .then(async response => {
-    if (response.status === 200) {
-      await mg.messages.create(DOMAIN, customerEmail)
-      .then(response => {
-        if (response.status === 200) {
-          res.status(200).json({ success: true })
+          varialCmsList = await client.collection('mailing_lists').findOne({ name: 'VarialCMS' })
         } else {
-          res.status(500).json({ error_message: 'client mail gun error', status: response.status, })
+          varialCmsList = await client.collection('mailing_lists').findOne({ name: 'VarialCMS' })
         }
-      })
-    } else {
-      res.status(500).json({ error_message: 'mail gun error', status: response.status, })
+    
+        const contactExists = await client.collection('contacts').countDocuments({ email: body.email.toLowerCase() })
+    
+        if (contactExists === 0) {
+          await client.collection('contacts').insertOne({
+            name: body.name,
+            email: body.email,
+            mailingLists: [varialCmsList._id]
+          })
+        } else {
+          const contact = await client.collection('contacts').findOne({ email: body.email.toLowerCase() })
+    
+          if (contact.mailingLists.some(i => JSON.stringify(i) === JSON.stringify(varialCmsList._id))) {
+            // do nothing
+          } else {
+            await client.collection('contacts').updateOne({ _id: contact._id }, {
+              $set: {
+                mailingLists: [...contact.mailingLists, varialCmsList._id]
+              }
+            })
+          }
+        }
     }
-  })
+
+    // Log contact request in Mongo
+    const contentModel = await client.collection('content_models').findOne({ 'name.plural': 'contact-requests' })
+
+    console.log('content model', contentModel)
+
+    await client.collection('contents').insertOne({
+      contentModel: contentModel._id,
+      fields: {
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+        city: body.city,
+        referredBy: body.referredBy,
+        comments: body.comments,
+      },
+      published: true,
+      createdAt: now,
+      updatedAt: now,
+      __v: 0,
+    })
+    // end of log contact request
+
+    const customerEmail = {
+      from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
+      to: body.email,
+      subject: 'Contact request received',
+      html: `
+        <h1>Your contact request has been received!</h1>
+        <p>Thank you for choosing Nicholson & Associates! Our team will be in touch with you soon.</p> 
+      `
+    }
+
+    const staffEmail = {
+      from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
+      to: process.env.MAIL_TO,
+      subject: `Contact request received at ${now}`,
+      html: `
+          <p>Date: ${now}</p>
+          <p>Name: ${body.name}</p>
+          <p>Email: ${body.email}</p>
+          <p>Phone: ${body.phone}</p>
+          <p>City: ${body.city}</p>
+          <p>Referred by: ${body.referredBy}</p>
+          <p>Questions/Comments: ${body.comments}</p>
+        `
+    }
+
+    await mg.messages.create(DOMAIN, staffEmail)
+    .then(async response => {
+      if (response.status === 200) {
+        await mg.messages.create(DOMAIN, customerEmail)
+        .then(async (response) => {
+          if (response.status === 200) {
+
+            
+
+
+            res.status(200).json({ success: true })
+          } else {
+            res.status(500).json({ error_message: 'client mail gun error', status: response.status, })
+          }
+        })
+      } else {
+        res.status(500).json({ error_message: 'mail gun error', status: response.status, })
+      }
+    })
+
+  } catch (error) {
+    console.error("Error:", error)
+  }
+
+  
 }
--- pages/api/forms/free-quote.js
+++ pages/api/forms/free-quote.js
@@ -1,6 +1,7 @@
 const Mailgun = require('mailgun.js')
 const formData = require('form-data')
 const mailgun = new Mailgun(formData)
+import { MongoClient } from 'mongodb'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(401).json({ error_message: 'Invalid request' })
@@ -13,43 +14,77 @@ export default async function handler(req, res) {
     key: API_KEY,
   })
 
-  const now = new Date()
+  const mongo = new MongoClient(process.env.MONGO_URI)
 
-  const customerEmail = {
-    from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
-    to: body.email,
-    subject: 'Free quote request received',
-    html: `
-      <h1>Your free quote request has been received!</h1>
-      <p>Thank you for choosing Nicholson & Associates! Our team will be in touch with you soon.</p> 
-    `
-  }
+  try {
+    await mongo.connect()
+    const client = mongo.db('VarialCMS')
 
-  const staffEmail = {
-    from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
-    to: process.env.MAIL_TO,
-    subject: `Free quote request received at ${now}`,
-    html: `
-        <p>Date: ${now}</p>
-        <p>Name: ${body.name}</p>
-        <p>Email: ${body.email}</p>
-        <p>Phone: ${body.phone}</p>
-        <p>City: ${body.city}</p>
-        <p>Referred by: ${body.referredBy}</p>
-        <p>Insurance Type: ${body.insuranceType}</p>
-        <p>Questions/Comments: ${body.comments}</p>
+    const now = new Date()
+
+    // Log contact request in Mongo
+    const contentModel = await client.collection('content_models').findOne({ 'name.plural': 'free-quote-requests' })
+
+    await client.collection('contents').insertOne({
+      contentModel: contentModel._id,
+      fields: {
+        name: body.name,
+        email: body.email,
+        phone: body.phone,
+        city: body.city,
+        referredBy: body.referredBy,
+        insuranceType: body.insuranceType,
+        comments: body.comments,
+      },
+      published: true,
+      createdAt: now,
+      updatedAt: now,
+      __v: 0,
+    })
+    // end of log contact request
+
+    const customerEmail = {
+      from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
+      to: body.email,
+      subject: 'Free quote request received',
+      html: `
+        <h1>Your free quote request has been received!</h1>
+        <p>Thank you for choosing Nicholson & Associates! Our team will be in touch with you soon.</p> 
       `
+    }
+
+    const staffEmail = {
+      from: `Nicholson & Associates <${process.env.MAIL_FROM}>`,
+      to: process.env.MAIL_TO,
+      subject: `Free quote request received at ${now}`,
+      html: `
+          <p>Date: ${now}</p>
+          <p>Name: ${body.name}</p>
+          <p>Email: ${body.email}</p>
+          <p>Phone: ${body.phone}</p>
+          <p>City: ${body.city}</p>
+          <p>Referred by: ${body.referredBy}</p>
+          <p>Insurance Type: ${body.insuranceType}</p>
+          <p>Questions/Comments: ${body.comments}</p>
+        `
+    }
+
+    await mg.messages.create(DOMAIN, staffEmail)
+    .then(async response => {
+      if (response.status === 200) {
+        await mg.messages.create(DOMAIN, customerEmail)
+        .then(response => {
+          if (response.status === 200) {
+            res.status(200).json({ success: true })
+          }
+        })
+      }
+    })
+
+  } catch (error) {
+    console.error("Error:", error)
   }
 
-  await mg.messages.create(DOMAIN, staffEmail)
-  .then(async response => {
-    if (response.status === 200) {
-      await mg.messages.create(DOMAIN, customerEmail)
-      .then(response => {
-        if (response.status === 200) {
-          res.status(200).json({ success: true })
-        }
-      })
-    }
-  })
+  
+
 }
